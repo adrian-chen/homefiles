85f52aab6cce46f4cc3912ed42ff350d9cd5f467
Mike Conigliaro <mike@conigliaro.org>
2015-02-02 23:29:12 -0700

    Install Flux via Homebrew

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ curl -L http://install.ohmyz.sh | sh
 which brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
 
+# Chrome
+brew cask install google-chrome
+
 # Sublime Text 3
 brew tap caskroom/versions
 brew cask install sublime-text3
@@ -18,3 +21,6 @@ brew cask install iterm2
 
 # rbenv
 brew install rbenv ruby-build
+
+# Flux
+brew cask install flux
