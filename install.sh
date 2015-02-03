951236d215a526e35ba76c5bb8962d1bb185fd06
Mike Conigliaro <mike@conigliaro.org>
2015-02-02 22:17:21 -0800

    Install Sublime Text 3 and iTerm2 via Homebrew

--- install.sh
+++ install.sh
@@ -6,7 +6,15 @@ set -eu
 curl -L http://install.ohmyz.sh | sh
 
 # Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+which brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install caskroom/cask/brew-cask
+
+# Sublime Text 3
+brew tap caskroom/versions
+brew cask install sublime-text3
+
+# iTerm2
+brew cask install iterm2
 
 # rbenv
 brew install rbenv ruby-build
