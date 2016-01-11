7234e304c2fd9ab45184662eb4270ef1919868a7
Mike Conigliaro <mike@conigliaro.org>
2016-01-11 14:54:42 -0700

    Configure dock and screen saver

--- configure.sh
+++ configure.sh
@@ -11,12 +11,27 @@ function symlink {
   done
 }
 
-# Show hidden files in Finder
+# Show hidden files in finder
 defaults write com.apple.finder AppleShowAllFiles YES
 
-# Set keyboard repeat interval
+# Configure keyboard
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Configure dock
+defaults write com.apple.dock tilesize -int 32
+
+# Configure screen saver
+defaults write com.apple.screensaver "askForPassword" -int 1
+defaults write com.apple.screensaver "askForPasswordDelay" -int 5
+defaults -currentHost write com.apple.screensaver "idleTime" -int 300
+defaults -currentHost write com.apple.screensaver "moduleDict" -dict-add "moduleName" -string "Computer Name"
+defaults -currentHost write com.apple.screensaver "moduleDict" -dict-add "path" -string "/System/Library/Frameworks/ScreenSaver.framework/Resources/Computer Name.saver"
+defaults -currentHost write com.apple.ScreenSaver.Basic "MESSAGE" -string "Hey kid, I'm a computer."
+
+# Enable hot corner (start screen saver in bottom left)
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
 
 # Zsh settings
 symlink '.z*' "$HOME"
