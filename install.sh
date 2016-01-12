28e9a7bd9679afdefc3690c9dfc312840dcc1768
Mike Conigliaro <mike@conigliaro.org>
2016-01-12 11:23:47 -0700

    Remove brewdle

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ set -eu
 
 # Homebrew
 command -v brew >/dev/null 2>&1 || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap Homebrew/brewdler
-brew brewdle
+brew bundle
 
 # Homebrew uninstaller
 wget -nc -P /usr/local/bin https://gist.githubusercontent.com/mxcl/1173223/raw/a833ba44e7be8428d877e58640720ff43c59dbad/uninstall_homebrew.sh
